6e1e3eeab8148c415acb7491dd25e8f4d6c89576
Bowen Sun <bowensun@gmail.com>
2017-03-27 11:47:38 +0100

    * associate_public_ip_address using variable

--- nfs.tf
+++ nfs.tf
@@ -87,7 +87,7 @@ resource "aws_launch_configuration" "nfs" {
     delete_on_termination = false
   }
   user_data = "${data.template_file.nfs-cloudinit.rendered}"
-  associate_public_ip_address = true
+  associate_public_ip_address = "${var.openshift["internal"]}"
 
   lifecycle {
     create_before_destroy = true
