801e5de969402ea42aefeeb0cec4d4077dcad786
Bowen Sun <bowensun@gmail.com>
2017-11-21 15:36:42 +0000

    allow nodes to have outbound access

--- node-securitygroup.tf
+++ node-securitygroup.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "node" {
     security_groups = ["${aws_security_group.node-compute.id}"]
   }
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name = "${var.aws_conf["domain"]}-node"
     Stack = "${var.aws_conf["domain"]}"
